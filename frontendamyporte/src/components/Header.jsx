13f8016e40fd94673a8df47b3da8e8176ede39df
aziza-jbali <jbaliaziza32@gmail.com>
2026-06-11 00:46:44 +0200

    complete the header

--- frontendamyporte/src/components/Header.jsx
+++ frontendamyporte/src/components/Header.jsx
@@ -3,12 +3,12 @@ import {Link,useLocation} from "react-router-dom";
 import {useContext,useState} from "react";
 import {motion,AnimatePresence} from "framer-motion";
 import {HiMenu,HiX} from "react-icons/hi";
-import {Bell,Rocket,User,DoorOpen} from "lucide-react";
+import {Bell,Rocket,User,DoorOpen, Library, LogOut} from "lucide-react";
 
 import "../index.css"
  function Header() {
      const location=useLocation();
-     
+     const user=5;
      const navLinks=[
          {name:"Home",path:"/"},
          {name:"Invoices",path:"/invoices"},
@@ -26,7 +26,7 @@ import "../index.css"
           <span className='fff'>Amy porte</span>
           </motion.div>
          {/* Desktop menu */}
-         <div className=' hidden md:flex items-center gap-6'>
+         <div className='  md:flex items-center gap-6'>
           {navLinks.map((link)=>(<motion.div key={link.path} whileHover={{scale:1.1}}>
             <Link to={link.path} className={`relative text-sm font-medium transition-all duration-300 ${location.pathname === link.path ? "text-yellow-950":"text-neutral-700 hover:text-yellow-950"}`}>
             {Link.name}
@@ -34,6 +34,31 @@ import "../index.css"
             </Link>
 
           </motion.div>))}
+          { user ? (<div className="flex items-center gap-4">
+            {/* Notifications Icon */}
+            <Link to="/notifications">
+            <motion.div whileHover={{scale:1.2,rotate:10}} className="relative cursor-pointer">
+              <Bell size={22} className="text-yellow-950"/>
+              <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full animate-pulse"/>
+            </motion.div>
+            </Link>
+            {/* Profil Icon */}
+           <Link>
+            <motion.div whileHover={{scale:1.1}} className="relative cursor-pointr">
+              <User size={22} className="text-yellow-950"/>
+            </motion.div>
+           </Link>
+           {/* logout button */}
+           <button onClick={LogOut} className="ml-2 bg-linear-to-r from-yellow-900 to-orange-950 px-4 py-1.5 rounded-full font-medium text-white hover:shadow-lg hover:shadow-yellow-300/40 transition-all">
+           Logout</button>
+          </div>
+          ):(<div className="flex items-center gap-4">
+            <Link to="/login" className="text-neutral-700 hover:text-yellow-950 transition-all font-medium">
+            sign in</Link>
+            <Link to="/register" className="ml-2 bg-linear-to-r   from-yellow-400 to-orange-500 px-4 py-1.5 rounded-full font-medium  text-white hover:shadow-lg hover:shadow-yellow-300/40 transition-all">
+            sign up</Link>
+
+          </div>)}
          </div>
         </div>
 
