3a0a3aa92c5e933d1a43c8529776cf48ebe700ca
aziza-jbali <jbaliaziza32@gmail.com>
2026-06-18 23:23:25 +0200

    fixing input issues

--- frontendamyporte/src/components/Clientstep.jsx
+++ frontendamyporte/src/components/Clientstep.jsx
@@ -1,7 +1,22 @@
 import { Search } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {getallclients} from "../services/apiGestionclient.js"
+import { div } from 'framer-motion/client';
 export default function Clientstep() {
     const [query,setquery]=useState(" ")
+    const [clients,setclients]=useState([])
+    useEffect(()=>{
+      const fetchclient= async()=>{
+        try {
+          const response=await getallclients()
+          console.log(response)
+          setclients(response.data);
+        } catch (error) {
+          console.error("❌ Erreur lors de la récupération du client :", error);
+        }
+      }
+      fetchclient()
+    },[query])
   return (
     <div className='flex flex-col  min-h-screen  items-center justify-center fff '>
       
@@ -13,7 +28,10 @@ export default function Clientstep() {
      </div>
  {/* Search seggestion  */}
  <div className='w-77  bg-pink-800 rounded p-3'>
-   hello world
+   {clients.map((client)=>(
+    <p key={client._id}>{client.nom}</p>
+      
+   ))}
  </div>
 </div>
  
@@ -23,7 +41,29 @@ export default function Clientstep() {
 }
 
 
+//  useEffect(() => {
+//     const fetchClient = async () => {
+//       try {
+//         const response = await getallclient(); // جلب جميع العملاء
+//         const allClients = response.data;
+//         console.log("📋 Tous les clients:", allClients);
 
+//         // البحث مباشرة بعد جلب البيانات (وليس بعد setState)
+//         const foundClient = allClients.find(
+//           (c) => String(c._id).trim() === String(id).trim()
+//         );
+
+//         console.log("🎯 Client trouvé:", foundClient);
+//         setCustomer(foundClient || null);
+//       } catch (error) {
+//         console.error("❌ Erreur lors de la récupération du client :", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchClient();
+//   }, [id]);
 
 // المتصفح يسأل:
 
--- frontendamyporte/src/services/apiGestionclient.js
+++ frontendamyporte/src/services/apiGestionclient.js
@@ -1,2 +1,11 @@
 import axios from "axios";
-const apiUrl = "http://localhost:5011/clients";
+const apiUrl = "http://localhost:5001/clients";
+export async function getallclients(){
+    return  axios.get(`${apiUrl}/getclients`)
+}
+
+
+
+// export async function getallclient() {
+//   return await axios.get(`${apiUrl}/getClients`);
+// }
