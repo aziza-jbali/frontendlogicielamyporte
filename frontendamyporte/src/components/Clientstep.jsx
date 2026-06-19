376575a68c6b7c4dfbbef7087b539eab5610368c
aziza-jbali <jbaliaziza32@gmail.com>
2026-06-19 06:13:54 +0200

    ajouter un client

--- frontendamyporte/src/components/Clientstep.jsx
+++ frontendamyporte/src/components/Clientstep.jsx
@@ -2,6 +2,7 @@ import { Search } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import {getallclients} from "../services/apiGestionclient.js"
 import { div } from 'framer-motion/client';
+import { filter } from 'framer-motion/m';
 export default function Clientstep() {
     const [query,setquery]=useState(" ")
     const [clients,setclients]=useState([])
@@ -10,7 +11,10 @@ export default function Clientstep() {
         try {
           const response=await getallclients()
           console.log(response)
-          setclients(response.data);
+          const newarray=response.data.filter((client)=>  Object.values(client).includes(query))
+          console.log(newarray)
+          setclients(newarray);
+          console.log("i am working")
         } catch (error) {
           console.error("❌ Erreur lors de la récupération du client :", error);
         }
@@ -18,21 +22,28 @@ export default function Clientstep() {
       fetchclient()
     },[query])
   return (
-    <div className='flex flex-col  min-h-screen  items-center justify-center fff '>
-      
-<div className='flex flex-col gap-1 relative fff'>
-    <div className=' text-amber-50   relative fff '> 
-    <div style={{ border: "1px solid green " }} className='absolute fff top-2.5 text-2xl  mr-2'><i class="fa-solid fa-magnifying-glass "></i></div>
-     <input  onChange={(e)=>setquery(e.target.value)} type="search"  className='bg-pink-100 text-black fff p-3 rounded-2xl w-77 pl-8'/>
+    <div className='flex flex-col  min-h-screen  items-center justify-center    bg-linear-to-b from-[#faf6ef] to-[#f0e5d2] '>
+      <div className='flex flex-col relative gap-10 w-72 h-96 md:w-96 md:h-[500px] lg:w-[600px] lg:h-[700px] fff'>
+
+    <div className='  relative  flex flex-col gap-1 '> 
+    <div className='absolute   top-2.5 text-2xl left-0.5'><i class="fa-solid fa-magnifying-glass    "></i></div>
+     <input  onChange={(e)=>setquery(e.target.value)}  type="search"  className='bg-pink-100 text-black   p-3 rounded-2xl  pl-8'/>
      {console.log(query)}
-     </div>
- {/* Search seggestion  */}
- <div className='w-77  bg-pink-800 rounded p-3'>
-   {clients.map((client)=>(
-    <p key={client._id}>{client.nom}</p>
+      {/* Search seggestion  */}
+
+ <div className='rounded-xl overflow-hidden '>
+   {clients ?(clients.map((client)=>(
+    <div className=' bg-amber-50 p-2  '>     <div  className='ml-1 '><i class="fa-solid fa-magnifying-glass pr-4.5 "></i><span key={client._id}>{client.nom}</span></div>
+
+   </div>
+    
       
-   ))}
+   ))):""}
  </div>
+     </div>
+<div className='flex-1'></div>
+ <div className='fff'><p >Ajouter un client </p></div>
+  
 </div>
  
        
@@ -40,31 +51,3 @@ export default function Clientstep() {
   )
 }
 
-
-//  useEffect(() => {
-//     const fetchClient = async () => {
-//       try {
-//         const response = await getallclient(); // جلب جميع العملاء
-//         const allClients = response.data;
-//         console.log("📋 Tous les clients:", allClients);
-
-//         // البحث مباشرة بعد جلب البيانات (وليس بعد setState)
-//         const foundClient = allClients.find(
-//           (c) => String(c._id).trim() === String(id).trim()
-//         );
-
-//         console.log("🎯 Client trouvé:", foundClient);
-//         setCustomer(foundClient || null);
-//       } catch (error) {
-//         console.error("❌ Erreur lors de la récupération du client :", error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchClient();
-//   }, [id]);
-
-// المتصفح يسأل:
-
-// "من هو الأب الذي عنده position: relative لكي أتموضع بالنسبة له؟
