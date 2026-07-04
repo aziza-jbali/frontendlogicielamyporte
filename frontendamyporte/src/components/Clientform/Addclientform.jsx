14439227547cecb435d89f0ae0825f9f40c26b97
aziza-jbali <jbaliaziza32@gmail.com>
2026-07-04 11:45:45 +0200

    add client successfuly

--- frontendamyporte/src/components/Clientform/Addclientform.jsx
+++ frontendamyporte/src/components/Clientform/Addclientform.jsx
@@ -1,30 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { addclient } from "../../services/apiGestionclient";
+import { Trophy } from "lucide-react";
 export default function Addclientform() {
-   
+  const [clientdata, setclientdata] = useState({
+    nom: "",
+    numero: "",
+    adresse: "",
+  });
+  const handlesubmit=async()=>{
+    try {
+         const response =await addclient(clientdata)
+         setclientdata({nom:"",numero:"",adresse:""})
+         console.log("véhicule ajouté",response.data)
+    } catch (error) {
+          console.error("❌ خطأ أثناء إضافة السيارة:", error);
+      alert("حدث خطأ أثناء الإضافة.");
+    }
+
+  }
   return (
     <div className="w-full max-w-3xl mx-auto bg-white rounded-2xl shadow-lg p-8">
-        <h1 className='text-2xl font-bold text-[#614c38] mb-8 border-b pb-3'>
-            Configuration - Client
-        </h1>
-        <div className='flex flex-col gap-2 font-bold text-xl'>
-
-         <div className='flex flex-col  gap-2 font-bold text-xl'>
-            <label>Nom Complet :</label>
-            <input type="text" className='rounded-xl border border-gray-300 px-4 py-3 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-[#614c38]' />
-
+      <h1 className="text-2xl font-bold text-[#614c38] mb-8 border-b pb-3">
+        Configuration - Client
+      </h1>
+      <div className="flex flex-col gap-2 font-bold text-xl">
+        <div className="flex flex-col  gap-2 font-bold text-xl">
+          <label>Nom Complet :</label>
+          <input
+            type="text"
+            className="rounded-xl border border-gray-300 px-4 py-3 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-[#614c38]"
+            value={clientdata.nom}
+            onChange={(e) =>
+              setclientdata({
+                ...clientdata,
+                nom: e.target.value,
+              })
+            }
+          />
         </div>
-          <div className='flex flex-col font-bold text-xl'>
-            <label>Numéro :</label>
-            <input type="text" className='rounded-xl border border-gray-300 px-4 py-3 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-[#614c38]' />
-
+        <div className="flex flex-col gap-2 font-bold text-xl">
+          <label>Numéro :</label>
+          <input
+            type="text"
+            className="rounded-xl border border-gray-300 px-4 py-3 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-[#614c38]"
+              value={clientdata.numero}
+            onChange={(e) =>
+              setclientdata({
+                ...clientdata,
+                numero: e.target.value,
+              })
+            }
+          />
         </div>
-        <div className='flex flex-col font-bold text-xl '>
-            <label>Adresse :</label>
-            <input type="text" className='rounded-xl border border-gray-300 px-4 py-3 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-[#614c38]'/>
-
+        <div className="flex flex-col gap-2 font-bold text-xl ">
+          <label>Adresse :</label>
+          <input
+            type="text"
+            className="rounded-xl border border-gray-300 px-4 py-3 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-[#614c38]"
+              value={clientdata.adresse}
+            onChange={(e) =>
+              setclientdata({
+                ...clientdata,
+                adresse: e.target.value,
+              })
+            }
+          />
         </div>
+        <div className="flex justify-center items-center pb-2">
+            <button className="  duration-300  hover:bg-[#8C5A3C] hover:p-5 hover:rounded-2xl hover:shadow-2xl" onClick={()=>{handlesubmit()}}>Energistrer</button>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
--- frontendamyporte/src/services/apiGestionclient.js
+++ frontendamyporte/src/services/apiGestionclient.js
@@ -3,9 +3,17 @@ const apiUrl = "http://localhost:5001/clients";
 export async function getallclients(){
     return  axios.get(`${apiUrl}/getclients`)
 }
+export async function addclient(clientData){
+      const response = await axios.post(`${apiUrl}/createclient`, clientData);
+      return response.data;
 
+}
 
 
 // export async function getallclient() {
 //   return await axios.get(`${apiUrl}/getClients`);
+// }
+// export async function addClient(clientData) {
+//   const response = await axios.post(`${apiUrl}/createclient`, clientData);
+//   return response.data;
 // }
